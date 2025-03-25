421ae39113b26df133661b60f13b2a1956a63e7b
hyeonwoody <hyeonwoody@gmail.com>
2025-03-25 11:42:04 +0900

    🛢️DB : Add Status field in `MediaRecord`

--- golang/cmd/media/cmd/model/media.go
+++ golang/cmd/media/cmd/model/media.go
@@ -1,10 +1,14 @@
 package model
 
-import "time"
+import (
+	mntreamerModel "mntreamer/shared/model"
+	"time"
+)
 
 type MediaRecord struct {
-	PlatformId uint16    `gorm:"primaryKey;autoIncrement:false;uniqueIndex:idx_mediaRecord_platforId"`
-	StreamerId uint32    `gorm:"primaryKey;autoIncrement:false;uniqueIndex:idx_mediaRecord_streamerId"`
+	PlatformId uint16 `gorm:"primaryKey;autoIncrement:false;uniqueIndex:idx_mediaRecord_platforId"`
+	StreamerId uint32 `gorm:"primaryKey;autoIncrement:false;uniqueIndex:idx_mediaRecord_streamerId"`
+	Status     int8
 	Date       time.Time `gorm:"primaryKey;autoIncrement:false;uniqueIndex:idx_mediaRecord_date"`
 }
 
@@ -18,6 +22,7 @@ func NewMediaRecord(platformId uint16, streamerId uint32) *MediaRecord {
 	return &MediaRecord{
 		PlatformId: platformId,
 		StreamerId: streamerId,
+		Status:     mntreamerModel.IDLE,
 		Date:       time.Date(year, month, day, 0, 0, 0, 0, time.UTC),
 	}
 }
